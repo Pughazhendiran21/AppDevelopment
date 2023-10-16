39cc2c86b1ed101cd37d7e34b5a993c31feae1d1
Pughazhendiran21 <144771722+Pughazhendiran21@users.noreply.github.com>
2023-10-16 20:18:05 +0530

    Add files via upload

---
+++ Day-19/crud-staffs/src/main/java/com/syntaxErrors/crudstaffs/CrudStaffsApplication.java
@@ -0,0 +1,13 @@
+package com.syntaxErrors.crudstaffs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudStaffsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudStaffsApplication.class, args);
+	}
+
+}
---
+++ Day-19/crud-staffs/src/test/java/com/syntaxErrors/crudstaffs/CrudStaffsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syntaxErrors.crudstaffs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudStaffsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
